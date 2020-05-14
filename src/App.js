ad7e553d95563ef3728fca472f994f88d0bb4dea
Veronica Nerak <veronicanerak@gmail.com>
2020-05-14 15:20:39 -0400

    Book component updated and Books list added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from 'react';
 import "./styles/styles.scss";
 import Book from './Book'
 
+
+const books = [
+  {
+    "title": "Best book ever",
+    "image": "https://jbrary.com/wp-content/uploads/2018/03/the-dress-and-the-girl.jpg",
+    "price": "30",
+    "author": "June Marine"
+  },
+  {
+    "title": "All stars",
+    "image": "https://s26162.pcdn.co/wp-content/uploads/2019/01/9781616208882.jpg",
+    "price": "10",
+    "author": "Felipes Marine"
+  },
+  {
+    "title": "Welcome to the world",
+    "image": "https://3jqywe2zrpyl3iyev92tw7v6-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/the-fault-in-our-stars.jpg",
+    "price": "30",
+    "author": "June"
+  },
+  {
+    "title": "Dancing in the sunset",
+    "image": "https://s3.amazonaws.com/netgalley-covers/cover133358-medium.png",
+    "price": "50",
+    "author": "Seerich AJers"
+  },
+]
+
 const App = () => (
   <>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -17,13 +45,12 @@ const App = () => (
     </div>
   </div>
 
-  <div className="ed-grid m-grid-3">
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+  <div className="ed-grid m-grid-3">        
+
+    {
+      books.map( book => <Book titlle={book.title} image={book.image} price={book.price} author={book.author} /> )
+    }
+    
   </div>
   
   </>
--- src/Book.jsx
+++ src/Book.jsx
@@ -1,29 +1,43 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Book = () => (
+const Book = props => (
   <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://i.pinimg.com/236x/0c/ee/7e/0cee7e54fda8ac99ec11459448e89c7d.jpg" alt="" />
+        <img src={ props.image ? props.image : "https://www.skipprichard.com/wp-content/uploads/2017/12/9781492647966.jpg" } alt={props} />
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-          No Place like Here
-        </h3>
+        <h3 className="t5 s-mb-2 s-center">{ props.title }</h3>
         <div className="s-mb-2 s-main-center">
           <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
               <div className="circle img-container">
-                <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/78be1759-2624-4b20-b237-1bab3c934b2f.jpg" alt="author" />
+                <img src={props.image} alt={props.title} />
               </div>
             </div>
-            <span className="small">Christin June</span>
+            <span className="small">{`author: ${props.author}`}</span>
           </div>
         </div>
         <div className="s-main-center">
-          <a className="button--ghost-alert button--tiny" href="http://www.test.com">$ 20USD</a>
+          <a className="button--ghost-alert button--tiny" href="http://www.test.com">{`$ ${props.price} USD`}</a>
         </div>
       </div>
   </article>
 )
+
+Book.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  author: PropTypes.string
+}
+
+// Default Values
+Book.defaultProps = {
+  title: "There is no title",
+  image: "https://www.skipprichard.com/wp-content/uploads/2017/12/9781492647966.jpg",
+  price: "--",
+  author: "No author"
+}
 
 export default Book
